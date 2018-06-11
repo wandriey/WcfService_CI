65944f19265fbe24446bdecb4228e7a767168210
wandriey <jona434g@edu.easj.dk>
2018-06-11 12:32:04 +0200

    Test tilføjet

---
+++ WcfService_CITests/Service1Tests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WcfService_CI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfService_CI.Tests
+{
+    [TestClass()]
+    public class Service1Tests
+    {
+        [TestMethod()]
+        public void BeregnSumTest()
+        {
+            Service1 service = new Service1();
+
+            int udregning = service.BeregnSum(2, 5);
+
+            Assert.AreEqual(udregning, 7);
+        }
+    }
+}
